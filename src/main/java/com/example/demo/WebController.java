8eaba93a9e7899ef4a26275c68bc0673355ae124
Maweheb Belarbi <mawbelmawbel@gmail.com>
2021-02-18 18:46:12 +0100

    premier-commit:formulaire simple

---
+++ src/main/java/com/example/demo/WebController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Controller
+public class WebController implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/").setViewName("form");
+        registry.addViewController("/results").setViewName("results");
+
+    }
+    @GetMapping("/")
+    public String showForm(PersonForm personForm){
+        return "form";
+    }
+    @PostMapping("/")
+    public String checkInfo(PersonForm personForm){
+        return "redirect:/results";
+    }
+}
